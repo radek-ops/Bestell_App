cb169aafda5512231c3adfae927d946e25a62339
radek-ops <radek.network@gmail.com>
2025-12-08 21:23:56 +0100

    create  script.js in progress

--- script.js
+++ script.js
@@ -5,9 +5,483 @@ function toggleBurgerMenu() {
 }
 
 
-
-let ifeefgood = new Audio('/sounds/james-brown-i-feel-good.mp3')
+let ifeelfgood = new Audio('/sounds/james-brown-i-feel-good.mp3')
 function playSound() {
-    ifeefgood.play();
+    ifeelfgood.play();
 
 }
+
+
+let allDishes = [
+    {
+
+        "dish": true,
+        "title": "Pizza`s",
+        "selection": [
+            {
+                "name": "Pizza Margherita",
+                "description": "Mit Tomatensoße und Mozzarella",
+                "price": 8.50
+            },
+            {
+                "name": "Pizza Salami",
+                "description": "Mit Rindersalami und Zwiebeln",
+                "price": 9.50
+            },
+            {
+                "name": "Pizza Funghi",
+                "description": "Mit frischen Champignons",
+                "price": 9.00
+            },
+            {
+                "name": "Pizza Prosciutto",
+                "description": "Mit Vorderschinken",
+                "price": 9.50
+            },
+            {
+                "name": "Pizza Tonno", "description":
+                    "Mit Thunfisch und Zwiebeln",
+                "price": 10.50
+            },
+            {
+                "name": "Pizza Hawaii",
+                "description": "Mit Schinken und Ananas",
+                "price": 10.00
+            },
+            {
+                "name": "Pizza Quattro Formaggi",
+                "description": "Mit Mozzarella, Gorgonzola, Gouda und Parmesan",
+                "price": 11.00
+            },
+            {
+                "name": "Pizza Diavolo",
+                "description": "Scharf, mit Peperoniwurst und Jalapeños",
+                "price": 10.50
+            },
+            {
+                "name": "Pizza Calzone",
+                "description": "Zugeklappte Pizza mit Schinken und Pilzen",
+                "price": 10.50
+            },
+            {
+                "name": "Pizza Vegetaria",
+                "description": "Mit Paprika, Mais, Pilzen, Zwiebeln und Brokkoli",
+                "price": 10.00
+            }
+        ]
+    },
+    {
+
+        "dish": true,
+        "title": "Burger",
+        "selection": [
+            {
+                "name": "Hamburger",
+                "description": "180g Rindfleisch, Salat, Tomate, Gurke, Haussoße",
+                "price": 7.50
+            },
+            {
+                "name": "Cheeseburger",
+                "description": "Wie Hamburger, mit Cheddar Käse",
+                "price": 8.00
+            },
+            {
+                "name": "Bacon Burger",
+                "description": "Mit knusprigem Speck und BBQ-Soße",
+                "price": 9.00
+            },
+            {
+                "name": "Chili Burger",
+                "description": "Scharf, mit Jalapeños und Chuisoße",
+                "price": 8.50
+            },
+            {
+                "name": "Chicken Burger",
+                "description": "Knuspriges Hähnchenfilet mit Mayo",
+                "price": 8.00
+            },
+            {
+                "name": "Veggie Burger",
+                "description": "Gemüsepatty mit Avocado-Creme",
+                "price": 7.50
+            },
+            {
+                "name": "Double Burger",
+                "description": "Doppeltes Fleisch, doppelter Käse",
+                "price": 11.50
+            },
+            {
+                "name": "BBQ Onion Burger",
+                "description": "Mit geschmorten Zwiebeln und Rauchsoße",
+                "price": 9.50
+            },
+            {
+                "name": "Mushroom Swiss Burger",
+                "description": "Mit Pilzen und Schweizer Käse",
+                "price": 9.00
+            },
+            {
+                "name": "Hawaii Burger",
+                "description": "Mit gegrillter Ananas und Schinken",
+                "price": 8.50
+            }
+        ]
+    },
+    {
+
+        "dish": true,
+        "title": "Döner Gerichte",
+        "selection": [
+            {
+                "name": "Döner Kebab",
+                "description": "Drehspießfleisch im Fladenbrot mit Salat und Soße",
+                "price": 6.50
+            },
+            {
+                "name": "Döner Teller (Pommes)",
+                "description": "Fleisch, Pommes, Salat und Soße",
+                "price": 10.50
+            },
+            {
+                "name": "Döner Teller (Reis)",
+                "description": "Fleisch, Reis, Salat und Soße",
+                "price": 10.50
+            },
+            {
+                "name": "Dürüm Döner",
+                "description": "Gerolltes Yufka-Brot mit Fleisch und Salat",
+                "price": 7.00
+            },
+            {
+                "name": "Lahmacun (Salat)",
+                "description": "Türkische Pizza mit Salat und Soße",
+                "price": 6.00
+            },
+            {
+                "name": "Lahmacun (Fleisch)",
+                "description": "Türkische Pizza mit Dönerfleisch, Salat und Soße",
+                "price": 8.00
+            },
+            {
+                "name": "Döner Box",
+                "description": "Fleisch und Pommes in der Box",
+                "price": 6.00
+            },
+            {
+                "name": "Veggie Döner",
+                "description": "Im Brot mit Schafskäse und frittiertem Gemüse",
+                "price": 6.00
+            },
+            {
+                "name": "Jumbo Döner",
+                "description": "Extra viel Fleisch und extra großes Brot",
+                "price": 8.50
+            },
+            {
+                "name": "Iskender Kebab",
+                "description": "Fleisch auf Brotstücken mit Joghurt und Tomatensoße",
+                "price": 12.00
+            }
+        ]
+    },
+    {
+
+        "dish": true,
+        "title": "Chicken Gerichte",
+        "selection": [
+            {
+                "name": "Chicken Nuggets (9er)",
+                "description": "Mit Süß-Sauer Dip",
+                "price": 6.50
+            },
+            {
+                "name": "Chicken Wings (6er)",
+                "description": "Knusprige Flügel, leicht scharf",
+                "price": 7.00
+            },
+            {
+                "name": "Crispy Chicken Filets",
+                "description": "Panierte Hähnchenbruststreifen",
+                "price": 7.50
+            },
+            {
+                "name": "Hähnchenschnitzel Wiener Art",
+                "description": "Mit Pommes und Zitrone",
+                "price": 10.00
+            },
+            {
+                "name": "Rahmschnitzel",
+                "description": "Hähnchenschnitzel mit Rahmsoße und Reis",
+                "price": 11.50
+            },
+            {
+                "name": "Halbes Hähnchen",
+                "description": "Knusprig gegrillt (ohne Beilage)",
+                "price": 6.00
+            },
+            {
+                "name": "Chicken Curry",
+                "description": "Hähnchenbrustfilet in Currysoße mit Reis",
+                "price": 11.00
+            },
+            {
+                "name": "Chicken Salat",
+                "description": "Gemischter Salat mit gegrillten Hähnchenstreifen",
+                "price": 9.50
+            },
+            {
+                "name": "Chicken Wrap",
+                "description": "Wrap mit Crispy Chicken, Salat und Remoulade",
+                "price": 6.50
+            },
+            {
+                "name": "Hot Wings (9er)",
+                "description": "Extra scharfe Hähnchenflügel",
+                "price": 9.00
+            }
+        ]
+    },
+    {
+
+        "dish": true,
+        "title": "Extras (Belag)",
+        "selection": [
+            {
+                "name": "Extra Käse",
+                "description": "Zusätzliche Portion Gouda",
+                "price": 1.50
+            },
+            {
+                "name": "Extra Fleisch",
+                "description": "Zusätzliche Portion Fleisch",
+                "price": 2.50
+            },
+            {
+                "name": "Peperoni",
+                "description": "Mild oder scharf eingelegt",
+                "price": 1.00
+            },
+            {
+                "name": "Jalapeños",
+                "description": "Scharfe Chili-Scheiben",
+                "price": 1.00
+            },
+            {
+                "name": "Schafskäse",
+                "description": "Würfel vom Weichkäse",
+                "price": 1.50
+            },
+            {
+                "name": "Ananas",
+                "description": "Fruchtstücke",
+                "price": 1.00
+            },
+            {
+                "name": "Mais",
+                "description": "Süßer Mais",
+                "price": 1.00
+            },
+            {
+                "name": "Ei",
+                "description": "Gekochtes Ei in Scheiben",
+                "price": 1.00
+            },
+            {
+                "name": "Oliven",
+                "description": "Schwarze Oliven",
+                "price": 1.00
+            },
+            {
+                "name": "Röstzwiebeln",
+                "description": "Knusprige Zwiebeln",
+                "price": 0.50
+            }
+        ]
+    },
+    {
+
+        "dish": true,
+        "title": "Beilagen",
+        "selection": [
+            {
+                "name": "Pommes frites",
+                "description": "Klassisch, groß",
+                "price": 3.50
+            },
+            {
+                "name": "Süßkartoffelpommes",
+                "description": "Aus Süßkartoffeln",
+                "price": 4.50
+            },
+            {
+                "name": "Kroketten",
+                "description": "8 Stück",
+                "price": 3.50
+            },
+            {
+                "name": "Potato Wedges",
+                "description": "Kartoffelspalten gewürzt",
+                "price": 4.00
+            },
+            {
+                "name": "Reis",
+                "description": "Portion Butterreis",
+                "price": 3.00
+            },
+            {
+                "name": "Mozzarella Sticks",
+                "description": "6 Stück mit Dip",
+                "price": 5.50
+            },
+            {
+                "name": "Zwiebelringe",
+                "description": "8 Stück, paniert",
+                "price": 4.50
+            },
+            {
+                "name": "Kleiner Salat",
+                "description": "Beilagensalat mit Dressing",
+                "price": 4.00
+            },
+            {
+                "name": "Krautsalat",
+                "description": "Hausgemacht",
+                "price": 3.00
+            },
+            {
+                "name": "Knoblauchbrot",
+                "description": "Ciabatta mit Knoblauchbutter",
+                "price": 3.50
+            }
+        ]
+    },
+    {
+
+        "dish": true,
+        "title": "Soßen",
+        "selection": [
+            {
+                "name": "Ketchup",
+                "description": "Portionsbeutel",
+                "price": 0.50
+            },
+            {
+                "name": "Mayonnaise",
+                "description": "Salatmayonnaise",
+                "price": 0.50
+            },
+            {
+                "name": "Knoblauchsoße",
+                "description": "Hausgemacht, cremig",
+                "price": 1.00
+            },
+            {
+                "name": "Kräutersoße",
+                "description": "Joghurt-Kräuter",
+                "price": 1.00
+            },
+            {
+                "name": "Scharfe Soße",
+                "description": "Rote Chili-Soße",
+                "price": 1.00
+            },
+            {
+                "name": "BBQ Soße",
+                "description": "Rauchig",
+                "price": 0.80
+            },
+            {
+                "name": "Süß-Sauer Soße",
+                "description": "Asia Style",
+                "price": 0.80
+            },
+            {
+                "name": "Currysoße",
+                "description": "Kalt, fruchtig",
+                "price": 0.80
+            },
+            {
+                "name": "Cocktailsoße",
+                "description": "Cremig",
+                "price": 1.00
+            },
+            {
+                "name": "Sauce Hollandaise",
+                "description": "Warm",
+                "price": 1.50
+            }
+        ]
+    }
+]
+
+let allDishesArry = [];
+let showDishes = document.getElementById('dishes');
+let dishContentPrice = '';
+
+
+function init() {
+    dishArry();
+}
+
+
+function dishArry() {
+    showDishes.innerHTML = '';
+    allDishesArry = allDishes.filter((createArry) => { return createArry['dish'] });
+
+    for (let i = 0; i < allDishesArry.length; i++) {
+        let singelDish = allDishesArry[i];
+        let selection = loadDishes(i);
+        showDishes.innerHTML += selection;
+    }
+}
+
+
+function loadDishes(i) {
+
+    return `<button type="button" onclick="loadCurrentSelection(${i})">
+            <div class="main-card" id="card">
+            <h3>${allDishesArry[i].title}</h3>
+             </div></button>`;
+}
+
+
+function loadCurrentSelection(i) {
+    showDishes.innerHTML = '';
+
+    for (let j = 0; j < allDishes[i].selection.length; j++) {
+        const entireSelection = allDishes[i].selection[j];
+        let currentDishes = getcurrentDishes(entireSelection);
+        showDishes.innerHTML += currentDishes;
+    }
+
+
+    function getcurrentDishes(entireSelection) {
+        return `<div class="card" id="card">
+                       <div class="menu-card">
+                          <span class="dish-title" >${entireSelection.name}</span>
+                          <span class="dish-description" >${entireSelection.description}</span>
+                          <span class="dish-price" id="dishPrice" >Preis: ${entireSelection.price.toFixed(2).replace(".", ",")} €</span >
+                        </div> 
+                       <button id="plusDish" class="plus-button" type="button" onclick="addtoBasket()">+</button>                                       
+                </div >`;
+    }
+
+}
+
+
+
+
+function addtoBasket() {
+
+
+
+}
+
+
+
+
+
+
+
+
+
